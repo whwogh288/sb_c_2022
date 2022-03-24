4ca726d1042a8655d5413dcc5485ee0337be81ae
whwogh288 <jaehojo2731@gmail.com>
2022-03-24 18:08:50 +0900

    /usr/home/main2, /usr/home/main3 추가

--- src/main/java/com/jjh/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserHomeController.java
@@ -11,4 +11,16 @@ public class UserHomeController {
     public String showMain() {
     	return "안녕하세요";
     }
+    
+    @RequestMapping("usr/home/main2")
+    @ResponseBody
+    public String ddd() {
+    	return "반갑습니다.";
+    } 
+    
+    @RequestMapping("usr/home/main3")
+    @ResponseBody
+    public String aaa() {
+    	return "또만나요.";
+    }
 }
