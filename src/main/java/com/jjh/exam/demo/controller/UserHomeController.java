4aa88bc6d48d5bce0831156f8e89de824e6acd2c
whwogh288 <jaehojo2731@gmail.com>
2022-03-24 19:54:57 +0900

    액션메서드에서 각종 리턴타입 실험

--- src/main/java/com/jjh/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserHomeController.java
@@ -1,26 +1,108 @@
 package com.jjh.exam.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Controller
 public class UserHomeController {
-	private int count;
-	public UserHomeController() {
-		count = 0;
+	@RequestMapping("/usr/home/getString")
+	@ResponseBody
+	public String getString() {
+		return "HI";
+	}
+
+	@RequestMapping("/usr/home/getInt")
+	@ResponseBody
+	public int getInt() {
+		return 10;
+	}
+
+	@RequestMapping("/usr/home/getFloat")
+	@ResponseBody
+	public float getFloat() {
+		return 10.5f;
+	}
+
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 10.5;
+	}
+
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;
+	}
+
+	@RequestMapping("/usr/home/getCharacter")
+	@ResponseBody
+	public char getCharacter() {
+		return 'a';
+	}
+
+	@RequestMapping("/usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수나이", 22);
+		map.put("영희나이", 21);
+
+		return map;
+	}
+
+	@RequestMapping("/usr/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> List = new ArrayList<>();
+		List.add("철수");
+		List.add("영희");
+
+		return List;
 	}
 	
-    @RequestMapping("usr/home/getCount")
-    @ResponseBody
-    public int getCount() {
-    	return count++;
-    }
-    
-    @RequestMapping("usr/home/doSetCount")
-    @ResponseBody
-    public String doSetCount(int count) {
-    	this.count = count;
-    	return "count의 값이"+ this.count + "초과되었습니다.";
-    }
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article artricle = new Article(1, "제목1");
+
+		return artricle;
+	}
+	
+	@RequestMapping("/usr/home/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		Article artricle1 = new Article(1, "제목1");
+		Article artricle2 = new Article(2, "제목2");
+		
+		List<Article> List = new ArrayList<>();
+		List.add(artricle1);
+		List.add(artricle2);
+
+		return List;
+	}
+}
+
+@Data
+@AllArgsConstructor
+class Article {
+	
+	public int id;
+    public String title;
+
+	public Article() {
+		id = 1;
+		title = "제목1";
+	}
 }
