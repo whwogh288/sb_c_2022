ce3791984f69d75771cfd96b4744f632cb59f5b0
whwogh288 <jaehojo2731@gmail.com>
2022-03-28 05:44:19 +0900

    테스트 게시물 10개 자동생성

--- src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
@@ -17,7 +17,23 @@ public class UserArticleController {
 	public UserArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
- 
+		
+		makeTestData();
+		
+	}
+
+	private void makeTestData() {
+		for (int i = 1; i <=10; i++) {
+			int id = articlesLastId + 1;
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+			Article article = new Article(id, title, body);
+
+			articles.add(article);
+			articlesLastId = id;
+
+		}
+		
 	}
 
 	@RequestMapping("/usr/article/doAdd")
@@ -25,16 +41,23 @@ public class UserArticleController {
 	public Article doAdd(String title, String body) {
 		int id = articlesLastId + 1;
 		Article article = new Article(id, title, body);
-		
+
 		articles.add(article);
 		articlesLastId = id;
 
 		return article;
 	}
-	
+
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+
+		return id + "번 게시물을 삭제하였습니다.";
 	}
 }
