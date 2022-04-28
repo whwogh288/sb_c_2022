229586f1408e10aa294558ca3b09329139ac905f
whwogh288 <jaehojo2731@gmail.com>
2022-04-28 21:49:27 +0900

    액션 구현

--- src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
@@ -84,6 +84,17 @@ public class UserArticleController {
 	public List<Article> getArticles() {
 		return articles;
 	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = getArticle(id);
+		
+		if ( article == null ) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		return article;
+	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
