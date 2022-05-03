b1eaa3f39ff8c4dbdfa620dd8e7342e1ebb3ffd0
whwogh288 <jaehojo2731@gmail.com>
2022-05-03 23:12:10 +0900

    db/schema.sql 파일 추가

--- src/main/java/com/jjh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/jjh/exam/demo/repository/ArticleRepository.java
@@ -1,70 +1,20 @@
 package com.jjh.exam.demo.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.jjh.exam.demo.vo.Article;
 
-@Component
-public class ArticleRepository {
-	private int articlesLastId;
-	private List<Article> articles;
-	
-	
-	public ArticleRepository() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
+@Mapper
+public interface ArticleRepository {
+	public Article writeArticle(String title, String body);
 		
-		makeTestData();
-
-	}
+	public Article getArticle(int id);
+		
+	public void deleteArticle(int id);
 	
-	public void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목 " + i;
-			String body = "내용 " + i;
-
-			writeArticle(title, body);
-		}
-	}
-
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-		articlesLastId = id;
-
-		return article;
-	}
-
-	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
-
-	}
-
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
-
-	}
-
-	public List<Article> getArticles() {
-		return articles;
-	}
+	public void modifyArticle(int id, String title, String body);
+	
+	public List<Article> getArticles();
 }
