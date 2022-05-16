eaf67060df01afceec6a1a08fc64618524cf2613
whwogh288 <jaehojo2731@gmail.com>
2022-05-17 00:55:27 +0900

    /usr/article/doAdd 에 ResultData 적용

--- src/main/java/com/jjh/exam/demo/service/ArticleService.java
+++ src/main/java/com/jjh/exam/demo/service/ArticleService.java
@@ -2,11 +2,12 @@ package com.jjh.exam.demo.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jjh.exam.demo.repository.ArticleRepository;
+import com.jjh.exam.demo.util.Ut;
 import com.jjh.exam.demo.vo.Article;
+import com.jjh.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -16,9 +17,11 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getlastInsertId();
+		int id = articleRepository.getlastInsertId();
+		
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
 
 	public List<Article> getArticles() {
