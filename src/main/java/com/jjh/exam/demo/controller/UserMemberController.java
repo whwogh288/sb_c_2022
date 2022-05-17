d4e4ae67b1c5fe56f0df6d031b49713dcdf097ef
whwogh288 <jaehojo2731@gmail.com>
2022-05-18 00:16:38 +0900

    /usr/article/doAdd에 ResultData.newData 적용\

--- src/main/java/com/jjh/exam/demo/controller/UserMemberController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserMemberController.java
@@ -12,45 +12,45 @@ import com.jjh.exam.demo.vo.ResultData;
 @Controller
 public class UserMemberController {
 	private MemberService memberService;
-	
-public UserMemberController(MemberService memberService) {
-	this.memberService = memberService;
-}
-	
+
+	public UserMemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
 	@RequestMapping("/usr/member/dojoin")
 	@ResponseBody
-	public ResultData dojoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		 
-		 if (Ut.empty(loginId)) {
-			 return ResultData.from("F-1", "loginId(을)를 입력해주세요."); 
-		 }
-		 if (Ut.empty(loginPw)) {
-			 return ResultData.from("F-2", "loginPw(을)를 입력해주세요."); 
-		 }
-		 if (Ut.empty(name)) {
-			 return ResultData.from("F-3", "name(을)를 입력해주세요."); 
-		 }
-		 if (Ut.empty(nickname)) {
-			 return ResultData.from("F-4", "nickname(을)를 입력해주세요."); 
-		 }
-		 if (Ut.empty(cellphoneNo)) {
-			 return ResultData.from("F-5", "cellphoneNo(을)를 입력해주세요."); 
-		 }
-		 if (Ut.empty(email)) {
-			 return ResultData.from("F-6", "email(을)를 입력해주세요."); 
-		 }
-		 
-		 // S-1
-		 // 회원가입이 완료되었습니다.
-		 // Data
-		 ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		 
-		 if ( joinRd.isFail()) {
-			 return joinRd;
-		 }
-		 Member member = memberService.getMemberById((int)joinRd.getData1());
-		 
-		 return ResultData.newData(joinRd, member);
+	public ResultData dojoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+			String email) {
+
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-1", "loginId(을)를 입력해주세요.");
+		}
+		if (Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "loginPw(을)를 입력해주세요.");
+		}
+		if (Ut.empty(name)) {
+			return ResultData.from("F-3", "name(을)를 입력해주세요.");
+		}
+		if (Ut.empty(nickname)) {
+			return ResultData.from("F-4", "nickname(을)를 입력해주세요.");
+		}
+		if (Ut.empty(cellphoneNo)) {
+			return ResultData.from("F-5", "cellphoneNo(을)를 입력해주세요.");
+		}
+		if (Ut.empty(email)) {
+			return ResultData.from("F-6", "email(을)를 입력해주세요.");
+		}
+
+		// S-1
+		// 회원가입이 완료되었습니다.
+		// Data
+		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+		if (joinRd.isFail()) {
+			return joinRd;
+		}
+		Member member = memberService.getMemberById((int) joinRd.getData1());
+
+		return ResultData.newData(joinRd, member);
 	}
 }
-
