4c7adf3b0e53300863d9b7f00e5c7d96b61ee4fa
whwogh288 <jaehojo2731@gmail.com>
2022-05-31 21:39:40 +0900

    JSP 연결

--- src/main/java/com/jjh/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserHomeController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserHomeController {
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String getString() {
-		return "안녕하세요";
+	public String showMain() {
+		return "usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 }
 
