1d2fb9c6b9dc121e5f940d5017e84e05138afa4a
whwogh288 <jaehojo2731@gmail.com>
2022-06-02 16:56:39 +0900

    게시물 상세페이지 구현

--- src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
@@ -58,6 +58,15 @@ public class UserArticleController {
 		return "usr/article/list";
 	}
 	
+	@RequestMapping("/usr/article/detail")
+	public String showDetail(Model model, int id) {
+		Article article = articleService.getArticle(id);
+		
+		model.addAttribute("article", article);
+		
+		return "usr/article/detail";
+	}
+	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
 	public ResultData<Article> getArticle(int id) {
