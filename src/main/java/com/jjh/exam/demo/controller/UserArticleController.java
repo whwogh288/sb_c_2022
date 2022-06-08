f4fb9463f134381be21619a6014b1674d0aa1843
whwogh288 <jaehojo2731@gmail.com>
2022-06-08 19:16:42 +0900

    게시물 리스트 page 파라미터 처리

--- src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
@@ -2,11 +2,10 @@ package com.jjh.exam.demo.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jjh.exam.demo.service.ArticleService;
@@ -51,7 +50,7 @@ public class UserArticleController {
 	}
 
 	@RequestMapping("/usr/article/list")
-	public String showList(Model model, int boardId) {
+	public String showList(Model model, @RequestParam(defaultValue = "1")int boardId, @RequestParam(defaultValue = "1")int page) {
 		Board board = boardService.getBoardById(boardId);
 		
 		if ( board == null) {
@@ -59,7 +58,9 @@ public class UserArticleController {
 		}
 		
 		int articlesCount = articleService.getArticlesCount(boardId);
-		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId);
+		
+		int itemsCountInAPage = 10;
+		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId,itemsCountInAPage, page);
 		
 		model.addAttribute("board", board);
 		model.addAttribute("articlesCount", articlesCount);
--- src/main/java/com/jjh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/jjh/exam/demo/repository/ArticleRepository.java
@@ -43,9 +43,12 @@ public interface ArticleRepository {
 				AND A.boardId = #{boardId}
 			</if>
 			ORDER BY A.id DESC
+			<if test="limitStart != -1">
+				LIMIT #{limitStart}, #{limitTake}
+			</if>
 			</script>
 			""")
-    public List<Article> getArticles(@Param("boardId") int boardId);
+    public List<Article> getArticles(@Param("boardId") int boardId, int limitStart, int limitTake);
 
 	public int getlastInsertId();
 	
