133aea939cb33f8eb05feb82d8640d6ef2cfea78
whwogh288 <jaehojo2731@gmail.com>
2022-06-10 14:09:26 +0900

    게시물 검색어에 따라 게시물 필터, 게시물 페이지메뉴에 검색어 유지처리

--- src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserArticleController.java
@@ -61,7 +61,7 @@ public class UserArticleController {
 		
 		int itemsCountInAPage = 10;
 		int PagesCount = (int)Math.ceil((double)articlesCount / itemsCountInAPage);
-		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId,itemsCountInAPage, page);
+		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId, searchKeywordTypeCode, searchKeyword, itemsCountInAPage, page);
 		
 		model.addAttribute("boardId", boardId);
 		model.addAttribute("page", page);
